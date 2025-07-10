89bf16da917bc5b0e4ce761ab7397dbf7dcb996e
Ismael Bezi <ismael.bezi@gmail.com>
2025-07-09 22:24:40 -0300

    2025-07-09 - Primeiro vídeo carregado

--- src/modules/diagnostico/onboarding/etapa01_Categorias.jsx
+++ src/modules/diagnostico/onboarding/etapa01_Categorias.jsx
@@ -153,7 +153,7 @@ const CategoriasEtapa = ({
               <iframe
                 width="100%"
                 height="200"
-                src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+                src="https://www.youtube.com/embed/GBKcmAFiUf8"
                 title="Tutorial: Como organizar categorias financeiras"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
