55ded50c81a69c78d1916b095af771c57acab75c
Ismael Bezi <ismael.bezi@gmail.com>
2025-07-09 21:59:10 -0300

    2025-07-09 - Primeiro vídeo carregado

--- src/modules/diagnostico/onboarding/etapa00_IntroPercepcao.jsx
+++ src/modules/diagnostico/onboarding/etapa00_IntroPercepcao.jsx
@@ -232,7 +232,8 @@ const IntroPercepcaoEtapa = ({
               <iframe
                 width="100%"
                 height="200"
-                src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+                src="https://www.youtube.com/embed/GBKcmAFiUf8"
+
                 title="Introdução: Como funciona o diagnóstico financeiro"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
